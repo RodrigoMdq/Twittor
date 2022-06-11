e407dc336461220e8e634b6728069f5eab6d5f39
Rodrigo Fernandez <rodrigofernandezmdq@gmail.com>
2022-06-11 09:22:43 -0300

    Modificamos la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-
+var url = window.location.href;
+var swLocation = '/Twittor/sw.js';
 
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+
+    if(url.includes('localhost')){
+        var swLocation = '/sw.js';
+    }
+    navigator.serviceWorker.register( swLocation );
 }
 
 
